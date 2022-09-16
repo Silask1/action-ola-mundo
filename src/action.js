855adb4a3fb541bd7279b96ed35015e2f94103e8
Silask1 <silasafram@gmail.com>
2022-09-16 12:10:10 -0300

    Adiciona SCP

--- src/action.js
+++ src/action.js
@@ -1,5 +1,27 @@
+const { Client } = require('node-scp');
+
+
 async function run() {
+    debugger;
     console.log('Olá mundo');
+
+    try {
+        const client = await Client({
+            host: '20.120.4.78',
+            port: 22,
+            username: 'tqssolucoes',
+            password: 'Usrtqs8774636'
+        })
+
+        await client.uploadFile(
+            'README.md',
+            '/home/tqssolucoes/README.md'
+        );
+        client.close();
+        console.log('SCP Completado com Sucesso');
+    } catch (e) {
+        console.log(e);
+    }
 }
 
 run();
