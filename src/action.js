ba816d1d2ae27e0674af74d6599ba1c05c6fdebd
Silask1 <silasafram@gmail.com>
2022-10-31 09:50:41 -0300

    teste de secrets

--- src/action.js
+++ src/action.js
@@ -1,34 +1,41 @@
 const { Client } = require('node-scp');
 const core = require('@actions/core');
+const github = require('@actions/github');
 const path = require('path');
 
 
 async function run() {
     const source = core.getInput('source', { required: true });
     const target = core.getInput('target', { required: true });
+    const github_token = core.getInput('github_token', { required: true});
+    const run_number = core.getInput('run_number', {required: true})
 
-    console.log('Source: ' + source);
-    console.log('Target:' + target);
-    console.log('Targer Completo:' + path.join(target, source));
+    core.info("Run_Number Secret: " + run_number);
 
-    try {
-        const client = await Client({
-            host: '20.120.4.78',
-            port: 22,
-            username: 'tqssolucoes',
-            password: 'Usrtqs8774636'
-        });
-        console.log('Conectado');
+    //var octokit = github.getOctokit(github_token);
 
-        await client.uploadFile(
-            source,
-            path.join(target, source)
-        );
-        client.close();
-        console.log('SCP Completado com Sucesso');
-    } catch (e) {
-        console.log(e);
-    }
+    // console.log('Source: ' + source);
+    // console.log('Target:' + target);
+    // console.log('Targer Completo:' + path.join(target, source));
+
+    // try {
+    //     const client = await Client({
+    //         host: '20.120.4.78',
+    //         port: 22,
+    //         username: 'tqssolucoes',
+    //         password: 'Usrtqs8774636'
+    //     });
+    //     console.log('Conectado');
+
+    //     await client.uploadFile(
+    //         source,
+    //         path.join(target, source)
+    //     );
+    //     client.close();
+    //     console.log('SCP Completado com Sucesso');
+    // } catch (e) {
+    //     console.log(e);
+    // }
 }
 
 run();
